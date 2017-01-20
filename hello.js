9f40ef3a4cfd28af8b8b988c0c778045bf0198a6
愛睏 <sleepyknife@hepinxiandeMacBook-Pro.local>
2017-01-20 15:05:07 +0800

    modify node js

--- hello.js
+++ hello.js
@@ -3,4 +3,4 @@ var http = require('http')
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1> Hello World!<h1>');
-});
+}).listen(80);
