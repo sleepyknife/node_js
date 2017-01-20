b7870077af69d85b1941ff22cb0a2bd55020cc65
愛睏 <sleepyknife@hepinxiandeMacBook-Pro.local>
2017-01-20 14:01:37 +0800

    hello world

--- hello.js
+++ hello.js
@@ -1,12 +1,6 @@
-var express = require('express');
+var http = require('http')
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<h1> Hello World!<h1>');
+}).listen(80);
